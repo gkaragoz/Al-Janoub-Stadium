8cf0dc4a68e636e3603bff9b9b9d170d9c11852d
gkaragoz <gkaragoz96@gmail.com>
2019-09-01 14:40:04 +0300

    Finalize base scene.

---
+++ Al Janoub Stadium/Assets/Scripts/Laser.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+    private LineRenderer _lr;
+
+    private void Start() {
+        _lr = GetComponent<LineRenderer>();
+    }
+
+    private void Update() {
+        _lr.SetPosition(0, transform.position);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit)) {
+            if (hit.collider) {
+                _lr.SetPosition(1, hit.point);
+            }
+        } else _lr.SetPosition(1, transform.forward * 5000);
+    }
+
+}
